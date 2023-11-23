052227f439b438a484d1ba50340d75e6b02572e1
MrMinken <minimoto@hotmail.com>
2023-11-23 13:29:54 +0100

    med navbar

---
+++ main.js
@@ -0,0 +1,23 @@
+// common.js
+// Funktion för att hämta och inkludera navigationsmenyn
+function includeNavbar() {
+    // Skapa en ny XMLHttpRequest
+    var xhr = new XMLHttpRequest();
+
+    // Ange metod, URL och async-parameter
+    xhr.open('GET', 'navbar.html', true);
+
+    // Ange en callback-funktion för när förfrågan är klar
+    xhr.onload = function () {
+        // Om förfrågan är lyckad, lägg till navigationsmenyn till <body>
+        if (xhr.status >= 200 && xhr.status < 300) {
+            document.body.insertAdjacentHTML('afterbegin', xhr.responseText);
+        }
+    };
+
+    // Skicka förfrågan
+    xhr.send();
+}
+
+// Anropa funktionen för att inkludera navigationsmenyn
+includeNavbar();
