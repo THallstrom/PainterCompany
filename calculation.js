e303510136d81a546dd5517f991904fac7fffac9
kbardach <144119408+kbardach@users.noreply.github.com>
2023-12-14 08:42:08 +0100

    fix med margin

--- calculation.js
+++ calculation.js
@@ -2,8 +2,8 @@ document.getElementById('area-form').addEventListener('submit', function(event) 
     event.preventDefault();
 
     let area = document.getElementById('area').value;
-    let selectedPricePerSquareMeter = document.querySelector('input[name="priceOption"]:checked').value;
-    let totalPrice = area * selectedPricePerSquareMeter;
+    let selectedPriceOption = document.querySelector('input[name="priceOption"]:checked').value;
+    let totalPrice = area * selectedPriceOption;
 
     document.getElementById('result').textContent = 'Totalt pris: ' + totalPrice + ' kr';
 });
